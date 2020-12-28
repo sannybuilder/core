d001ea54d35d30629b856a30e1336abdd441b7d5
Seemann <mail@sannybuilder.com>
2020-12-27 21:52:49 -0500

    Add language server

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #![feature(never_type)]
 #![feature(bool_to_option)]
+#![feature(hash_raw_entry)]
 #[macro_use]
 pub mod common_ffi;
 pub mod dictionary;
+pub mod language_service;
 pub mod math;
 pub mod namespaces;
 pub mod parser;
